1bdfe0ebcef296c6a4d5aa9bc0d2656cce0ca731
yixuan <chenyixuan09@gmaill.com>
2020-07-13 00:15:42 -0400

    adding 3rd exercise

---
+++ mySql.py
@@ -0,0 +1,7 @@
+# mysql installation tutorial : https://www.dev2qa.com/how-to-use-mysql-on-mac/
+
+# **** (use if needed) **** python set to 3.7.3 in mac: https://opensource.com/article/19/5/python-3-default-mac
+
+# Python mysql tutorial: https://www.w3schools.com/python/python_mysql_getstarted.asp
+
+# TODO : design table schema, put resources/customer_rating_movies into 6 tables in local mysql database using python programming
